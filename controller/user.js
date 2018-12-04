2399c9168c75e07bbc74c692538a1da6debc969b
Li Rui <lirui_working@163.com>
2018-12-04 16:29:22 +0800

    first test.

--- controller/user.js
+++ controller/user.js
@@ -6,8 +6,16 @@ const getTestInfo = async ctx =>{
     ctx.body = userInfo;
     ctx.status = 200;
 };
+const getUserInfo = async ctx =>{
+    let id  = ctx.query.id;
+    let userInfo = await UserModel.getUserInfoById(id);
+    ctx.body = userInfo;
+    ctx.status = 200;
+};
+
 module.exports.routers = {
-    'GET /getTestInfo':getTestInfo
+    'GET /getTestInfo':getTestInfo,
+    'GET /getUserInfo':getUserInfo
 };
 module.exports.securedRouters = {
 
--- model/user.js
+++ model/user.js
@@ -3,9 +3,14 @@ const connection=require('../index');
 const getTestById=(id)=>{
     let _sql = `SELECT * FROM user_info WHERE id = ${id}`;
     return connection.query(_sql);
+};
 
-}
+const getUserInfoById=(id)=>{
+    let _sql = `SELECT * FROM user WHERE id = ${id}`;
+    return connection.query(_sql);
+};
 
 module.exports={
-    getTestById
+    getTestById,
+    getUserInfoById
 };
