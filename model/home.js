292b3ee13a7b98e5a9f78a745ec6f819b9476f0d
Li Rui <lirui_working@163.com>
2018-12-04 21:11:34 +0800

    getMonthInfo.

--- model/home.js
+++ model/home.js
@@ -1,7 +1,7 @@
 'use strict';
 const connection=require('../index');
 const getMonthInfo=(year, month)=>{
-    let _sql = `SELECT * FROM time_schedule WHERE year = ${year} && month = ${month}`;
+    let _sql = `SELECT day, status FROM time_schedule WHERE year = ${year} && month = ${month} && status = 1`;
     return connection.query(_sql);
 };
 
