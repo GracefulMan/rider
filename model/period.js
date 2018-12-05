b34634665a5875a0a968fc169453b05ceaa5a42e
Li Rui <lirui_working@163.com>
2018-12-05 22:47:47 +0800

    getDayTodo.

--- model/period.js
+++ model/period.js
@@ -1,7 +1,7 @@
 'use strict';
 const connection=require('../index');
 const addPeriod=(uid, userphone, timeid, year, month, day, time, car)=>{
-    let _sql = `INSERT INTO rider_period (uid, userphone, timeid, year, month, day, time, car, status)VALUES(${uid}, ${userphone}, ${timeid}, ${year}, ${month}, ${day}, ${time}, ${car}, 1);`;
+    let _sql = `INSERT INTO rider_period (uid, userphone, timeid, year, month, day, time, car, status)VALUES(${uid}, ${userphone}, ${timeid}, ${year}, ${month}, ${day}, "${time}", ${car}, 1);`;
     return connection.query(_sql);
 };
 
