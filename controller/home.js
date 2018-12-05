1d9f0977179855aa35a7ae6de3dc0639051b8ffd
Li Rui <lirui_working@163.com>
2018-12-05 15:49:44 +0800

    getDayTodo.

--- controller/home.js
+++ controller/home.js
@@ -7,19 +7,30 @@ const getMonthInfo = async ctx =>{
     ctx.body = monthInfo;
     ctx.status = 200;
 };
-const getDayInfo = async ctx =>{
+const getDayTodo = async ctx =>{
     let year = ctx.request.body.year;
     let month = ctx.request.body.month;
     let day = ctx.request.body.day;
-    let dayInfo = await HomeModel.getDayInfo(year, month, day);
-    ctx.body = dayInfo;
+    let uid = ctx.request.body.uid;
+    let dayTodo = await HomeModel.getDayTodo(year, month, day);
+    let userDayTodo = await HomeModel.getUserDayTodo(year, month, day, uid);
+    let dayTodoNew = [];
+    for (let i=0; i<userDayTodo.length; i++) {
+        let timeid = userDayTodo[i].timeid;
+        for (let j=0; j<dayTodo.length; j++) {
+            if (dayTodo[j].id != timeid) {
+                dayTodoNew.push(dayTodo[j]);
+            } ;
+        } ;
+    };
+    ctx.body = dayTodoNew;
     ctx.status = 200;
 };
 
 
 module.exports.routers = {
     'POST /getMonthInfo':getMonthInfo,
-    'POST /getDayInfo':getDayInfo,
+    'POST /getDayInfo':getDayTodo,
 
 };
 module.exports.securedRouters = {
--- model/home.js
+++ model/home.js
@@ -4,12 +4,17 @@ const getMonthInfo=(year, month)=>{
     let _sql = `SELECT DISTINCT day FROM time_schedule WHERE year = ${year} && month = ${month} && status = 1`;
     return connection.query(_sql);
 };
-const getDayInfo=(year, month, day)=>{
+const getDayTodo=(year, month, day)=>{
     let _sql = `SELECT * FROM time_schedule WHERE year = ${year} && month = ${month} && day = ${day} && status = 1`;
+    return connection.query(_sql);
+};
+const getUserDayTodo=(year, month, day, uid)=>{
+    let _sql = `SELECT * FROM rider_period WHERE uid = ${uid} && year = ${year} && month = ${month} && day = ${day} && (status = 1 || status = 3)`;
     return connection.query(_sql);
 };
 
 module.exports={
     getMonthInfo,
-    getDayInfo
+    getDayTodo,
+    getUserDayTodo
 };
