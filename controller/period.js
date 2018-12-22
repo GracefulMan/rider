f590e96b8617637ea9a5ab1459a75b3f2cbf229e
Li Rui <lirui_working@163.com>
2018-12-22 15:23:39 +0800

    Schedule.

--- controller/period.js
+++ controller/period.js
@@ -31,8 +31,9 @@ const addPeriod = async ctx =>{
 // 取消班次
 const deletePeriod = async ctx =>{
     let id  = ctx.request.body.id;
-    let timeid = ctx.request.body.timeid;
     await PeriodModel.deletePeriod(id);  // 删除班次
+    let period = await PeriodModel.getPeriod(id);
+    let timeid = period[0].timeid;
     let schedule = await ScheduleModel.getScheduleById(timeid);
     let num = schedule[0].num_signed - 1;
     await ScheduleModel.changeScheduleNumSigned(timeid, num);  // 班次报名人数-1
