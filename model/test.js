e61e45dba1f636eaa444d538186bdf478543f2ed
Li Rui <lirui_working@163.com>
2018-12-26 20:19:20 +0800

    TEST SQL.

--- model/test.js
+++ model/test.js
@@ -38,7 +38,7 @@ const changeScheduleStatus=(id, status)=>{
 const test2=(startTime, endTime, numNeeded)=>{
     let _sql = `INSERT INTO public.schedule (start_time, end_time, num_needed, num_signed, status)VALUES
     ("${startTime}", "${endTime}", ${numNeeded}, 0, 1);`;
-    return connection.query(_sql);
+    return connection.pgSQL(_sql);
 };
 
 // DELETE
